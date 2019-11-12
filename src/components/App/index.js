af7d41cfa797153cc46b9be712b519f12798c87b
krzysztofbogusz91 <krzysztofbogusz050591@gmail.com>
2019-11-12 16:54:25 +0100

    Sets basic app view

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,13 +5,37 @@ import styles from './style.scss';
 class App extends Component {
   state = {
     team: [],
+    list: [],
   };
-
+  componentDidMount() {
+    fetch('https://swapi.co/api/people/')
+      .then(resp => resp.json())
+      .then((listOfHeroes) => {
+        console.log(listOfHeroes);
+        this.setState({ list: listOfHeroes.results });
+      }, (err) => {
+        console.log(err);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
   render() {
+    const heroesList = this.state.list.map(person => (<li key={person.created}>{person.name}</li>));
+    const chosenList = this.state.team.map(person => (<li key={person.created}>{person.name}</li>));
     return (
       <div className={styles.app}>
         <h1 className={styles.header}> Gather the team before setting off </h1>
+        <div>
+          <label htmlFor="hero-inputs"> Search for heroes: <input /> </label>    
+        </div>
+        <ul>
+          {heroesList}
+        </ul>
+        <ul>
+          {chosenList.length > 0 ? chosenList : (<h2>No one chosen...</h2>)}
+        </ul>
       </div>
     );
   }
